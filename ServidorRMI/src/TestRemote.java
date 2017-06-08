74a4a25b00486756276ccf6e4efe92f5e2c84131
fernando <feranimaciones@gmail.com>
2017-06-08 12:45:35 -0600

    Version 1.0 08/06/2017

---
+++ ServidorRMI/src/TestRemote.java
@@ -0,0 +1,16 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mario
+ */
+public interface  TestRemote extends Remote {
+      String sayHello(String name) throws RemoteException;
+}
